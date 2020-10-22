30378d8ffe15ee8f8ba0c9dfe2368c14e70461ef
root <root@static.234.83.243.136.clients.your-server.de>
2020-10-22 18:38:53 -0400

    Upload asset files

--- src/timer.js
+++ src/timer.js
@@ -1,52 +1,37 @@
-// (function () {
-//
-//     const $window = $(window),
-//         $sectionOne = $('#section_one'),
-//         $serviceOne = $('#service_one'),
-//         $serviceTwo = $('#service_two'),
-//         $serviceThree = $('#service_three'),
-//         $serviceBtnOne = $('#service_btn_one'),
-//         $sectionTwo = $('#section_two'),
-//         $discountResultOne = $('#discount_result_one'),
-//         $discountResultTwo = $('#discount_result_two'),
-//         $discountResultThree = $('#discount_result_three'),
-//         $discountResultFour = $('#discount_result_four'),
-//         $sectionThree = $('#section_three'),
-//         $discountBoxLeft = $('.discount_box_left'),
-//         $discountBoxCenter = $('.discount_box_center'),
-//         $discountBoxRight = $('.discount_box_right'),
-//         $cartBtn = $('.cart-btn'),
-//         $serviceBtnTwo = $('#service_btn_two');
-//
-//
-//
-//     $window.scroll(function () {
-//         // section one
-//         if ($window.scrollTop() > $sectionOne.position().top - 500) {
-//             $serviceOne.delay(200).animate({top: '0px', opacity: '1'},{duration:300});
-//             $serviceTwo.delay(400).animate({top: '0px', opacity: '1'}, {duration:300});
-//             $serviceThree.delay(600).animate({top: '0px', opacity: '1'}, {duration:300});
-//             $serviceBtnOne.delay(800).animate({opacity: '1'}, {duration:300});
-//             $serviceBtnTwo.delay(800).animate({opacity: '1'}, {duration:300});
-//         }
-//
-//         // section two
-//         if ($window.scrollTop() > $sectionTwo.position().top - 500) {
-//             $discountResultOne.delay(200).animate({left: '0px', opacity: '1'}, {duration:300});
-//             $discountResultTwo.delay(400).animate({left: '0px', opacity: '1'}, {duration:300});
-//             $discountResultThree.delay(600).animate({left: '0px', opacity: '1'}, {duration:300});
-//             $discountResultFour.delay(800).animate({left: '0px', opacity: '1'}, {duration:300});
-//         }
-//
-//         // section two
-//         if ($window.scrollTop() > $sectionThree.position().top - 500) {
-//             $discountBoxLeft.delay(200).animate({left: '0px', opacity: '1'}, {duration:500});
-//             $discountBoxCenter.delay(200).animate({opacity: '1'}, {duration:500});
-//             $discountBoxRight.delay(200).animate({right: '0px', opacity: '1'}, {duration:500});
-//
-//             $cartBtn.addClass('shadow-animation');
-//         }
-//     })
-//
-//
-// })();
+(function () {
+
+    const $second = $('#second_time'),
+        $minutes = $('#minutes_time'),
+        $hours = $('#hours_time'),
+        $day = $('#day_time');
+
+        const date_future = new Date(2020, 9, 30);
+
+    setInterval(function () {
+        const date_now = new Date();
+
+        let seconds = Math.floor((date_future - (date_now)) / 1000),
+            minutes = Math.floor(seconds / 60),
+            hours = Math.floor(minutes / 60),
+            days = Math.floor(hours / 24);
+
+        hours = hours - (days * 24);
+        minutes = minutes - (days * 24 * 60) - (hours * 60);
+        seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
+
+        if (date_now.getDate() >= date_future.getDate()) {
+
+            $day.text('0');
+            $second.text('0');
+            $minutes.text('0');
+            $hours.text('0');
+
+            return
+        }
+
+        $day.text(days);
+        $second.text(seconds);
+        $minutes.text(minutes);
+        $hours.text(hours);
+    }, 1000);
+})();
