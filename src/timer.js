5163e000fb82a182c820343e7662670a13164495
Max Base <MaxBaseCode@Gmail.Com>
2020-11-06 18:56:48 +0330

    Modify timer to 2020, 11, 20

--- src/timer.js
+++ src/timer.js
@@ -5,7 +5,7 @@
         $hours = $('#hours_time'),
         $day = $('#day_time');
 
-        const date_future = new Date(2020, 9, 30);
+        const date_future = new Date(2020, 11, 20);
 
     setInterval(function () {
         const date_now = new Date();
